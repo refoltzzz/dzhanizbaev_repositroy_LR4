669a7a63ad9c0c99e003ee6f5c6730effab4549b
german <germandzanaev97@gmail.com>
2025-04-09 23:28:50 +0300

    create111

---
+++ dzhanizbaev_repository_lr4_0.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <limits>
+#include <string>
+using namespace std;
+
+int check(string str = "", int minx = INT_MIN, int maxx = INT_MAX) 
+{
+    int x;
+    cout << str << endl;
+    while (!(cin >> x)  x < minx  x > maxx) 
+    {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cout << "neverno vvedite zanovo" << endl;
+        cout << str << endl;
+    }
+    return x;
+}
+
+void A()
+{
+    
+}
+
+void B()
+{
+    
+}
+
+void slozhenie()
+{   
+    
+}
+
+void vichitanie()
+{
+    
+}
+
+int main() 
+{
+    int nomer_zadaniya = 0;
+    while (true) 
+    {
+        cout << "nomer 1. vvesti A\n";
+        cout << "nomer 2. vvesti B\n";
+        cout << "nomer 3. vipolnit operaciu slozhenia(+)\n";
+        cout << "nomer 4. vipolnit operaciu vichitaniya(-)\n"; 
+        cout << "esli xotite zakrit programy, vvedite 0\n";
+        nomer_zadaniya = check("vvedite nomer zadaniya", 0, 4);
+
+        if (nomer_zadaniya == 0) 
+        {
+            break;
+        }
+
+        switch (nomer_zadaniya) 
+        {
+            case 1:
+                A();
+                break;
+            case 2:
+                B();
+                break;
+            case 3:
+                slozhenie();
+                break;
+            case 4:
+                vichitanie();
+                break;
+        }
+
+        cout << "rabota vipolnena. zakonchit rabotu programi? (1 - da, 0 - net): ";
+        int exit_vibor = check("", 0, 1);
+        if (exit_vibor == 1) 
+        {
+            break;
+        }
+    }
+    cout << "programma zakryta." << endl;
+    return 0;
+}
